662a74992d234b5d7197448afea768490ec353d9
hai.liting <hailiting@example.com>
2020-11-16 10:10:54 +0800

    暂存

--- lib/dioFuture/ui/page/splash.dart
+++ lib/dioFuture/ui/page/splash.dart
@@ -66,4 +66,187 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin{
           .login(loginName: phone,password: password);
     }
   }
+  @override
+  void dispose(){
+    _logoController.dispose();
+    _countdownController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context){
+    ScreenUtil.init(context, designSize: Size(750, 1334), allowFontScaling: true);
+    return Scaffold(
+      body: WillPopScope(
+        onWillPop: ()=>Future.value(false),
+        child: Stack(fit: StackFit.expand,children: <Widget>[
+          Image.asset(
+            ImageHelper.wrapAssets("main_bg"),
+            fit: BoxFit.fill
+          ),
+          AnimatedFlutterLogo(
+            animation: _animation
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SafeArea(
+              child: InkWell(
+                onTap: (){
+                  nextPage(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                  margin: EdgeInsets.only(right: 20, bottom: 20),
+                  child: AnimatedCountdown(
+                    context: context,
+                    animation: StepTween(begin: 3, end: 0).animate(_countdownController),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              child: InkWell(
+                onTap: (){
+
+                },
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  alignment: AlignmentDirectional.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: ScreenWH.rsw(133),
+                        height: ScreenWH.rsh(1),
+                        color: Colours.color_white,
+                        margin: EdgeInsets.only(right: ScreenWH.rsw(30)),
+                      ),
+                      Text('-----', style: TextStyle(color: Colours.color_white, fontSize: ScreenWH.rsSp(24)),),
+                      Container(
+                        margin: EdgeInsets.only(right: ScreenWH.rsw(30)),
+                        width: ScreenWH.rsw(133),
+                        height: ScreenWH.rsh(1),
+                        color: Colours.color_white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],),
+      ),
+    );
+  }
+}
+class AnimatedCountdown extends AnimatedWidget{
+  final Animation<int> animation;
+  AnimatedCountdown({key, this.animation, context}):super(key: key, listenable: animation){
+    this.animation.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        nextPage(context);
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Text("", style: TextStyle(color: Colors.white),);
+  }
+}
+class AnimatedFlutterLogo extends AnimatedWidget {
+  AnimatedFlutterLogo({
+    Key key,
+    Animation<double> animation,
+}):super(key:  key, listenable: animation);
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return AnimatedAlign(
+      duration:Duration(milliseconds: 10),
+      alignment: Alignment(0, animation.value*0.3),
+      curve: Curves.bounceOut,
+      child: Image.asset(
+        ImageHelper.wrapAssets("common/app_logo"),
+        width: ScreenWH.rsw(180),
+        height: ScreenWH.rsw(180),
+      ),
+    );
+  }
+}
+
+class AnimatedAndroidLoge extends AnimatedWidget {
+  AnimatedAndroidLoge({
+    Key key,
+    Animation<double> animation
+}):super(key: key, listenable: animation);
+  @override
+  Widget build(BuildContext context) {
+final Animation<double> animation = listenable;
+return Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: <Widget>[
+    Image.asset(
+      ImageHelper.wrapAssets("common/app_loge"),
+      width: ScreenWH.rsw(180)*(1-animation.value),
+      height: ScreenWH.rsw(180)*(1-animation.value),
+    ),
+  ],
+);
+  }
+}
+void nextPage(context){
+  Application.router.navigateTo(context, Routes.mainPage, replace: true, transition: TransitionType.native);
+}
+class GuidePage extends StatefulWidget{
+  static const List<String> images = <String>[
+    "guide_page_1.png",
+    "guide_page_2.png",
+    "guide_page_3.png",
+    "guide_page_4.png",
+  ];
+  @override
+  _GuidePageState createState()=>_GuidePageState();
+}
+class _GuidePageState extends State<GuidePage>{
+  int curIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).primaryColor,
+        child: Stack(
+          alignment: Alignment(0, 0.87),
+          children: [
+            Swiper(
+              itemBuilder: (ctx, index)=>Image.asset(
+                "assets/images/${GuidePage.images[index]}",
+                fit: BoxFit.fill,
+              ),
+              itemCount: GuidePage.images.length,
+              loop: false,
+              onIndexChanged: (index){
+                setState(() {
+                  curIndex=index;
+                });
+              },
+            ),
+            Offstage(
+              offstage: curIndex!=GuidePage.images.length -1,
+              child: CupertinoButton(
+                color: Theme.of(context).primaryColorDark,
+                child: Text("点我开始"),
+                onPressed: (){
+                  nextPage(context);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
